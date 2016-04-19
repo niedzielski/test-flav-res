4b7ffb86cc85e2620d2bbb1767061ae3246afd79
Stephen Niedzielski <stephen@niedzielski.com>
2016-04-19 00:03:38 -0600

    Add test resource and update test
    Output is:
    
      prod=bar
      test=foo

--- app/src/androidTest/java/com/niedzielski/android/testflavres/ApplicationTest.java
+++ app/src/androidTest/java/com/niedzielski/android/testflavres/ApplicationTest.java
@@ -1,13 +1,16 @@
 package com.niedzielski.android.testflavres;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
+import android.test.InstrumentationTestCase;
+import android.util.Log;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+public class ApplicationTest extends InstrumentationTestCase {
+    private static final String TAG = ApplicationTest.class.getName();
+
+    public void testResources() {
+        Log.d(TAG, "prod=" + getInstrumentation().getTargetContext().getString(R.string.prod_res));
+        Log.d(TAG, "test=" + getInstrumentation().getContext().getString(com.niedzielski.android.testflavres.test.R.string.test_res));
     }
 }
